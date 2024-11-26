61a62f4cf997d041a3320be7f3bccf3c0721d53c
haxy <zaidsaheb4@gmail.com>
2024-11-26 19:16:13 +0100

    changed code structure a bit

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -7,57 +7,65 @@ from opencensus.ext.azure.log_exporter import AzureLogHandler
 import logging
 from datetime import timedelta
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///local.db'
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.permanent_session_lifetime = timedelta(days=1)  # Set session lifetime
+def create_app(test_config=None):
+    app = Flask(__name__)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    if test_config is None:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///local.db'
+        app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+        app.permanent_session_lifetime = timedelta(days=1)
 
-# Configure CORS to allow credentials and specify allowed origins for production
-if os.getenv('ENV') == 'production':
-    CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:8080"}})
-else:
-    # For local development
-    CORS(app, supports_credentials=True, resources={
-        r"/*": {
-            "origins": "http://localhost:8080",
-            "allow_headers": ["Content-Type", "Authorization", "x-access-token"],  # Allow x-access-token header
-            "expose_headers": ["Access-Control-Allow-Origin"],
-            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-        }
-    })
+        # Select environment based on the ENV environment variable
+        if os.getenv('ENV') == 'local':
+            print("Running in local mode")
+            app.config.from_object('config.LocalConfig')
+        elif os.getenv('ENV') == 'dev':
+            print("Running in development mode")
+            app.config.from_object('config.DevelopmentConfig')
+        elif os.getenv('ENV') == 'uat':
+            print("Running in uat mode")
+            app.config.from_object('config.UATConfig')
+        elif os.getenv('ENV') == 'ghci':
+            print("Running in github mode")
+            app.config.from_object('config.GithubCIConfig')
+    else:
+        app.config.update(test_config)
 
-# Select environment based on the ENV environment variable
-if os.getenv('ENV') == 'local':
-    print("Running in local mode")
-    app.config.from_object('config.LocalConfig')
-elif os.getenv('ENV') == 'dev':
-    print("Running in development mode")
-    app.config.from_object('config.DevelopmentConfig')
-elif os.getenv('ENV') == 'ghci':
-    print("Running in github mode")
-    app.config.from_object('config.GithubCIConfig')
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-# Configure Azure Application Insights
-app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = os.environ.get('APPINSIGHTS_INSTRUMENTATIONKEY')
+    # Configure CORS
+    if os.getenv('ENV') == 'production':
+        CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:8080"}})
+    else:
+        CORS(app, supports_credentials=True, resources={
+            r"/*": {
+                "origins": "http://localhost:8080",
+                "allow_headers": ["Content-Type", "Authorization", "x-access-token"],
+                "expose_headers": ["Access-Control-Allow-Origin"],
+                "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+            }
+        })
 
-if app.config['APPINSIGHTS_INSTRUMENTATIONKEY']:
-    handler = AzureLogHandler(connection_string=f'InstrumentationKey={app.config["APPINSIGHTS_INSTRUMENTATIONKEY"]}')
-    logger = logging.getLogger(__name__)
-    logger.addHandler(handler)
-    logger.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
+    # Configure Azure Application Insights
+    app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = os.environ.get('APPINSIGHTS_INSTRUMENTATIONKEY')
 
-from iebank_api.models import Account, User, Transaction
+    if app.config['APPINSIGHTS_INSTRUMENTATIONKEY']:
+        handler = AzureLogHandler(connection_string=f'InstrumentationKey={app.config["APPINSIGHTS_INSTRUMENTATIONKEY"]}')
+        logger = logging.getLogger(__name__)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+        app.logger.addHandler(handler)
 
-with app.app_context():
-    # Uncomment to add the country column to the account table
-    # query = text("ALTER TABLE account ADD COLUMN country VARCHAR(32)")
-    # db.session.execute(query)
-    # db.session.commit()
+    from iebank_api.models import Account, User, Transaction
 
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
-from iebank_api import routes
+    from iebank_api import routes
+
+    return app
+
+# Initialize SQLAlchemy and Migrate outside of create_app
+db = SQLAlchemy()
+migrate = Migrate()

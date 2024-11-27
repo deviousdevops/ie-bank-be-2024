4ec59a622924079041b4c6ecf583cae0b7931d04
haxy <zaidsaheb4@gmail.com>
2024-11-27 08:32:56 +0100

    try to swithc to gunicorn

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from iebank_api import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
